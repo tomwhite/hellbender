9f389ab00498c1c62529e96ee2abf9d9b87757f3
Tom White <tom.e.white@gmail.com>
2018-08-27 14:29:06 +0100

    Use Spark files to distribute the reference for SparkWalker classes.

--- src/main/java/org/broadinstitute/hellbender/tools/HaplotypeCallerSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/HaplotypeCallerSpark.java
@@ -97,9 +97,6 @@ public final class HaplotypeCallerSpark extends GATKSparkTool {
         @Argument(fullName="read-shard-size", doc = "Maximum size of each read shard, in bases. For good performance, this should be much larger than the maximum assembly region size.", optional = true)
         public int readShardSize = DEFAULT_READSHARD_SIZE;
 
-        @Argument(fullName="read-shard-padding", doc = "Each read shard has this many bases of extra context on each side. Read shards must have as much or more padding than assembly regions.", optional = true)
-        public int readShardPadding = HaplotypeCaller.DEFAULT_ASSEMBLY_REGION_PADDING;
-
         @Argument(fullName = AssemblyRegionWalker.MIN_ASSEMBLY_LONG_NAME, doc = "Minimum size of an assembly region", optional = true)
         public int minAssemblyRegionSize = HaplotypeCaller.DEFAULT_MIN_ASSEMBLY_REGION_SIZE;
 
@@ -246,7 +243,7 @@ public final class HaplotypeCallerSpark extends GATKSparkTool {
 
         final Broadcast<VariantAnnotatorEngine> annotatorEngineBroadcast = ctx.broadcast(variantannotatorEngine);
 
-        final List<ShardBoundary> shardBoundaries = getShardBoundaries(header, intervals, shardingArgs.readShardSize, shardingArgs.readShardPadding);
+        final List<ShardBoundary> shardBoundaries = getShardBoundaries(header, intervals, shardingArgs.readShardSize);
 
         final int maxReadLength = reads.map(r -> r.getEnd() - r.getStart() + 1).reduce(Math::max);
 
@@ -292,9 +289,9 @@ public final class HaplotypeCallerSpark extends GATKSparkTool {
      * based on the -L intervals
      */
     private static List<ShardBoundary> getShardBoundaries(final SAMFileHeader
-        header, final List<SimpleInterval> intervals, final int readShardSize, final int readShardPadding) {
+        header, final List<SimpleInterval> intervals, final int readShardSize) {
         return intervals.stream()
-            .flatMap(interval -> Shard.divideIntervalIntoShards(interval, readShardSize, readShardPadding, header.getSequenceDictionary()).stream())
+            .flatMap(interval -> Shard.divideIntervalIntoShards(interval, readShardSize, 0, header.getSequenceDictionary()).stream())
             .collect(Collectors.toList());
     }
 
